cf6938b3472388a369bebc4f2c1183f35f83b978
Jhonnier20 <jhonnier023@hotmail.com>
2026-04-26 15:48:09 -0500

    Add backend

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,1 +1,8 @@
-
+terraform {
+  backend "s3" {
+    bucket       = "serverless-basic-terraform-state"
+    key          = "terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
+}

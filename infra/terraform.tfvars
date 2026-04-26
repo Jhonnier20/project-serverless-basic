6357d87fe7516e9fc2e426dabf348b84568c29c9
Jhonnier20 <jhonnier023@hotmail.com>
2026-04-26 15:54:16 -0500

    Add backend

--- infra/terraform.tfvars
+++ infra/terraform.tfvars
@@ -1,9 +1,1 @@
 region_main = "us-east-1"
-
-name_terraform_state_bucket_s3 = "serverless-basic-terraform-state"
-tags_terraform_state_s3 = {
-  Name        = "My bucket for state Terraform"
-  Environment = "Dev"
-  Owner       = "Jhonnier"
-}
-status_versioning_s3 = "Enabled"
--- infra/variables.tf
+++ infra/variables.tf
@@ -2,19 +2,3 @@ variable "region_main" {
   description = "Region  main for project"
   type        = string
 }
-
-#################### BACKEND ####################
-variable "name_terraform_state_bucket_s3" {
-  description = "Name for Bucket"
-  type        = string
-}
-variable "tags_terraform_state_s3" {
-  description = "Tags del s3 state"
-  type        = map(string)
-}
-variable "status_versioning_s3" {
-  description = "Habiltar el versionamiento en el S3"
-  type        = string
-}
-
-###################################################

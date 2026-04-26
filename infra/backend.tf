384d35bd29ebf095957f55240312891ba2c8fea0
Jhonnier20 <jhonnier023@hotmail.com>
2026-04-26 15:32:56 -0500

    First deploy backend

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,8 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket         = "serverless-basic-terraform-state"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "serverless-table-state"
-  }
-}
+

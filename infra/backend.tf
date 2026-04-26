daf8541db206b2413b605b44cec5ebd6c12e5fb5
Jhonnier20 <jhonnier023@hotmail.com>
2026-04-26 15:46:19 -0500

    First deploy backend

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,8 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket       = "serverless-basic-terraform-state"
-    key          = "terraform.tfstate"
-    region       = "us-east-1"
-    use_lockfile = true
-  }
-}
+
